edf8bb31016dc2b7d622fb11fe1746d4d7bbe906
engr-rafiur <rsrafirahman@gmail.com>
2026-03-05 06:08:04 +0600

    views register update

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # django-authenticate
+## deafult user model
+```
+| Field        | Type          | Description           |
+| ------------ | ------------- | --------------------- |
+| username     | CharField     | Required, unique      |
+| first_name   | CharField     | Optional              |
+| last_name    | CharField     | Optional              |
+| email        | EmailField    | Optional              |
+| password     | CharField     | Hashed password       |
+| is_staff     | BooleanField  | Admin site access     |
+| is_active    | BooleanField  | Active user or not    |
+| is_superuser | BooleanField  | Full permissions      |
+| last_login   | DateTimeField | Last login time       |
+| date_joined  | DateTimeField | Account creation time |
+
+```
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,49 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect 
+from django.contrib.auth.models import User # default model import
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash #
+from django.contrib import messages # show msg 
 
 # Create your views here.
 def home(request):
     return render(request, 'accounts/index.html')
 
 def register(request):
+
+    if request.method == 'POST':
+        user_name = request.POST.get('user_name') 
+        firstname = request.POST.get('first_name') 
+        lastname = request.POST.get('last_name') 
+        mail = request.POST.get('email') 
+        password = request.POST.get('password') 
+        confirm_password = request.POST.get('confirm_password') 
+
+        if password != confirm_password:
+            messages.error(request, 'wrong password')
+            return redirect('register')
+        
+        if User.objects.filte(username = user_name).exists():
+            messages.error(request, 'username already exists')
+            return redirect('register')
+        
+        if User.objects.filter(email = mail).exists():
+            messages.error(request, 'mail already exists')
+            return redirect('register')
+        
+        # Create user with hased password for using create_user()
+        user = User.objects.create_user(
+            username = user_name,  # model = variable
+            email = mail,
+            password = password 
+        ) 
+
+        user.first_name = firstname  # user.model = variable  
+        user.last_name = lastname 
+
+        user.save()
+        messages.success(request, 'account created succesfully')
+        return redirect('login')
+
+
     return render(request, 'accounts/register.html')
 
 def signin(request):
